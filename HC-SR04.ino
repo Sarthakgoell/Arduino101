fba3cada31408a2ba1a24b2103c206cf919749bc
Abhyuday1997 <abhyuday964808@gmail.com>
2017-09-05 18:38:01 +0530

    Add files via upload

---
+++ HC-SR04.ino
@@ -0,0 +1,25 @@
+
+long duration;
+float distance;
+
+
+void setup() {
+pinMode(13, OUTPUT);
+pinMode(11, INPUT);
+Serial.begin(9600);
+}
+
+void loop() {
+  digitalWrite(13, LOW);
+  delayMicroseconds(2);
+
+  digitalWrite(13, HIGH);
+  delayMicroseconds(10);
+
+  duration = pulseIn(11, HIGH);
+
+  distance = duration*0.03356*0.5 ;
+Serial.print("Distance in cm : ");
+Serial.println(distance);
+delay(1000);
+}
---
+++ Tachometer.ino
@@ -0,0 +1,25 @@
+
+long t ; 
+const int out = 7;
+float rpm;
+
+void setup() 
+{
+ 
+  
+  pinMode(out, INPUT);
+  Serial.begin(9600);
+  
+
+  
+
+}
+
+void loop() {
+  t = pulseIn(out, LOW);
+ rpm = 60000*0.5*1000/t ;
+
+ Serial.print("RPM : ");
+ Serial.println(rpm);
+delay(1000);
+}
